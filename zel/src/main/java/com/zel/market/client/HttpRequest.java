dc53c35dcc6471ebfb2efad437253c4ce675942a
caoziyao <wyzycao@gmail.com>
2020-12-09 19:47:09 +0800

    'request'

--- zel/src/main/java/com/zel/market/client/HttpRequest.java
+++ zel/src/main/java/com/zel/market/client/HttpRequest.java
@@ -2,15 +2,19 @@ package com.zel.market.client;
 
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.UnsupportedEncodingException;
+import java.util.*;
 
 import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 public class HttpRequest {
@@ -68,7 +72,67 @@ public class HttpRequest {
         return result;
     }
 
-    public String postData(String url) {
-        return null;
+    public String post(String url, Map<String, Object> paramMap) {
+        String result = "";
+        CloseableHttpResponse response = null;
+
+        CloseableHttpClient httpClient = HttpConnectionManager.getHttpClient();
+        HttpPost httpPost = new HttpPost(url);
+        // 设置请求头信息，鉴权
+        httpPost.setHeader("Authorization", "Bearer da3efcbf-0845-4fe3-8aba-ee040be542c0");
+        // 设置配置请求参数
+        RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(35000)// 连接主机服务超时时间
+                .setConnectionRequestTimeout(35000)// 请求超时时间
+                .setSocketTimeout(60000)// 数据读取超时时间
+                .build();
+        // 为httpPost实例设置配置
+        httpPost.setConfig(requestConfig);
+        // 执行 post 请求得到返回对象
+        // 封装post请求参数
+        if (null != paramMap && paramMap.size() > 0) {
+            List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+            // 通过map集成entrySet方法获取entity
+            Set<Map.Entry<String, Object>> entrySet = paramMap.entrySet();
+            // 循环遍历，获取迭代器
+            Iterator<Map.Entry<String, Object>> i$ = entrySet.iterator();
+            while (i$.hasNext()) {
+                Map.Entry<String, Object> mapEntry = i$.next();
+                nvps.add(new BasicNameValuePair(mapEntry.getKey(), mapEntry.getValue().toString()));
+            }
+
+            // 为httpPost设置封装好的请求参数
+            try {
+                httpPost.setEntity(new UrlEncodedFormEntity(nvps, "UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            response = httpClient.execute(httpPost);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // 通过返回对象获取返回数据
+        HttpEntity entity = response.getEntity();
+        // 通过EntityUtils中的toString方法将结果转换为字符串
+        try {
+            result = EntityUtils.toString(entity);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // 关闭资源
+            try {
+                response.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                httpClient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
     }
 }
---
+++ zel/src/test/java/com/zel/market/client/HttpRequestTest.java
@@ -0,0 +1,20 @@
+package com.zel.market.client;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HttpRequestTest {
+
+    @Test
+    void get() {
+        String s = new HttpRequest().get("http://www.baidu.com");
+        System.out.println(s);
+    }
+
+    @Test
+    void post() {
+        String s = new HttpRequest().post("http://www.baidu.com", null);
+        System.out.println(s);
+    }
+}
