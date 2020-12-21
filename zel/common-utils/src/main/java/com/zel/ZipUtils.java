c6e7c9726ea493f2e495158046766f93373896c1
caoziyao <wyzycao@gmail.com>
2020-12-21 20:37:26 +0800

    filter

--- zel/common-utils/src/main/java/com/zel/ZipUtils.java
+++ zel/common-utils/src/main/java/com/zel/ZipUtils.java
@@ -1,16 +1,29 @@
 package com.zel;
 
 import java.util.zip.Deflater;
+import java.util.zip.Inflater;
 
 /**
  * 数据压缩
  */
 public class ZipUtils {
 
+    /**
+     * 压缩
+     */
     public static byte[] compress(byte[] data) {
         byte[] output = new byte[0];
 
         Deflater compresser = new Deflater();
+        return null;
+    }
+
+    /**
+     * 解压缩
+     *
+     */
+    public static byte[] decompress(byte[] data) {
+        Inflater decompresser = new Inflater();
         return null;
     }
 }
