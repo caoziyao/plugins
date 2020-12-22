e1c057e9b550ea7e1ff24a2753c84e60570c34ca
caoziyao <wyzycao@gmail.com>
2020-12-22 20:50:28 +0800

    add bd

--- zel/common-utils/src/main/java/com/zel/commonutils/DateUtil.java
+++ zel/common-utils/src/main/java/com/zel/commonutils/DateUtil.java
@@ -52,6 +52,18 @@ public class DateUtil {
         return calendar.getTime();
     }
 
+    public Date getNightDay(Date day) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(day);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+
+        calendar.add(Calendar.DAY_OF_WEEK, 1);
+        return calendar.getTime(); //当天 24 点
+    }
+
     /**
      * 昨天凌晨
      */
--- zel/common-utils/src/main/java/com/zel/commonutils/MathUtils.java
+++ zel/common-utils/src/main/java/com/zel/commonutils/MathUtils.java
@@ -2,7 +2,6 @@ package com.zel.commonutils;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.text.DecimalFormat;
 
 public class MathUtils {
     public static void main(String[] args) {
@@ -12,18 +11,19 @@ public class MathUtils {
 
     /**
      * rate = a / b
+     *
      * @param a
      * @param b
      * @return
      */
-     static String rate(int a, int b) {
+    static String rate(int a, int b) {
         if (b == 0) {
             return "0%";
         }
-        
-         BigDecimal bd = new BigDecimal((float) a / b * 100);
-         String s1 = bd.setScale(2, RoundingMode.HALF_UP) + "%";
-         System.out.println(s1);
-         return s1;
+
+        BigDecimal bd = new BigDecimal((float) a / b * 100);
+        String s1 = bd.setScale(2, RoundingMode.HALF_UP) + "%";
+        System.out.println(s1);
+        return s1;
     }
 }
