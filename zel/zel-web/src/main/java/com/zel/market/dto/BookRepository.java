2da10b9d7aad9024bed0576d633a18c5d6f07745
caoziyao <wyzycao@gmail.com>
2020-12-28 23:26:36 +0800

    feat(es):  demo

--- zel/zel-web/src/main/java/com/zel/market/dto/BookRepository.java
+++ zel/zel-web/src/main/java/com/zel/market/dto/BookRepository.java
@@ -1,9 +1,9 @@
-package com.zel.market.dto;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Page;
-import org.springframework.data.elasticsearch.annotations.Query;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Component;
+//package com.zel.market.dto;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.elasticsearch.annotations.Query;
+//import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+//import org.springframework.stereotype.Component;
 
 /**
  * Description:
@@ -12,10 +12,10 @@ import org.springframework.stereotype.Component;
  * @version 1.0.0
  * @since 2020/9/26
  */
-@Component
-public interface BookRepository  extends ElasticsearchRepository<Book,Integer> {
-    Page<Book> findByCatory(String catory, Pageable pageable);
-
-    @Query("{ \"bool\":{ \"must\":[ { \"multi_match\": { \"query\": \"?0\", \"type\": \"most_fields\", \"fields\": [ \"title\", \"content\" ] } }, { \"match\": { \"catory\": \"?1\" } } ] } } ")
-    Page<Book> searchByKeyWordsAndCatory(String keyword, String catory, Pageable pageable);
-}
+//@Component
+//public interface BookRepository  extends ElasticsearchRepository<Book,Integer> {
+//    Page<Book> findByCatory(String catory, Pageable pageable);
+//
+//    @Query("{ \"bool\":{ \"must\":[ { \"multi_match\": { \"query\": \"?0\", \"type\": \"most_fields\", \"fields\": [ \"title\", \"content\" ] } }, { \"match\": { \"catory\": \"?1\" } } ] } } ")
+//    Page<Book> searchByKeyWordsAndCatory(String keyword, String catory, Pageable pageable);
+//}
