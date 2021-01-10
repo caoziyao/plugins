02053e64fbdb3a3469f6bab85ce614329533241c
caoziyao <wyzycao@gmail.com>
2021-01-10 20:39:48 +0800

    feat():

--- zel/zel-web/src/main/java/com/zel/market/controller/TestController.java
+++ zel/zel-web/src/main/java/com/zel/market/controller/TestController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
+ 
 @RestController
 @RequestMapping(value = "/test")
 public class TestController {
