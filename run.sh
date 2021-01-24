f05efd06d0b292dfae577ad3a320f16bea4e444b
caoziyao <wyzycao@gmail.com>
2021-01-24 23:57:18 +0800

    review

---
+++ run.sh
@@ -0,0 +1,24 @@
+clean() {
+  #清空之前的启动日志
+  #echo "" > $./nohup.out
+}
+
+shop() {
+  echo '-----stop-------'
+  sh ./stop.sh
+}
+
+restart() {
+  echo '-----start-------'
+  sh ./startup.sh
+}
+
+main() {
+  clean
+  shop
+  restart
+}
+
+main
+
+
