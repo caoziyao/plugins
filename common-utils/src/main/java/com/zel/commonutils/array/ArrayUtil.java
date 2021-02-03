85d828f003113f31d3eb342c11f6abb3f694341d
caoziyao <wyzycao@gmail.com>
2021-02-03 17:22:30 +0800

    ae

--- common-utils/src/main/java/com/zel/commonutils/array/ArrayUtil.java
+++ common-utils/src/main/java/com/zel/commonutils/array/ArrayUtil.java
@@ -1,4 +1,16 @@
 package com.zel.commonutils.array;
 
 public class ArrayUtil {
+    public static void main(String[] args) {
+        int i = 1;
+        int b = 2;
+        int c = 10;
+        while (i < b) {
+            c = c + 1;
+            i = i + 1;
+        }
+        System.out.println(i);
+        System.out.println(c);
+
+    }
 }
