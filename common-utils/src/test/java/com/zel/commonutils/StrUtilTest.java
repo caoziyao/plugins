d752b6ac2857b3b74114ce46d16159f943a170fa
caoziyao <wyzycao@gmail.com>
2021-02-18 15:24:25 +0800

    test

--- common-utils/src/test/java/com/zel/commonutils/StrUtilTest.java
+++ common-utils/src/test/java/com/zel/commonutils/StrUtilTest.java
@@ -4,33 +4,33 @@ import com.zel.commonutils.client.RequestUtil;
 import junit.framework.TestCase;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class StrUtilTest extends TestCase {
 
     @Test
     public void testTestFormat() {
-        String a = "logTime=1611123769890";
-//        System.out.println(a.length());
+        List<Integer> ls = new ArrayList<>();
+        ls.add(3);
+        ls.add(4);
+        System.out.println(isSymArray(ls));
 
-        int index = a.indexOf("logTime");
-        System.out.println(index);
+    }
 
-//        RequestUtil.getParam(a, "logTime")
-//        String b = a.substring(0, index -1 );
-//        System.out.println(b);
-//        System.out.println( a.indexOf("logTime"));
-//
-//        String c = a.substring(index + "logTime".length() + 1 , index + "logTime".length() + 1 + 13 );
-//        System.out.println(c);
-//        String format = StrUtil.format("hell {} {}", "abc", "ccc");
-//        System.out.println(format);
-//
-//        Map<String, Object> map = new HashMap<>();
-//        map.put("name", "zhangsan");
-//        map.put("age", 18);
-//
-//        System.out.println(StrUtil.format("hello {name} {age}", map));
+    public boolean isSymArray(List<Integer> ls) {
+        if (ls.size() == 1) {
+            return true;
+        }
+
+        for (int i = 0; i < ls.size() / 2; i++) {
+            int index = ls.size() - i - 1;
+            if (ls.get(i) != ls.get(index)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
