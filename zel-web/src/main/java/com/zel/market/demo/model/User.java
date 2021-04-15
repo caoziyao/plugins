2163edc4033f4d51f05f2675f333494bac5e063e
caoziyao <wyzycao@gmail.com>
2021-04-15 16:00:37 +0800

    mysql gen

---
+++ zel-web/src/main/java/com/zel/market/demo/model/User.java
@@ -0,0 +1,63 @@
+package com.zel.market.demo.model;
+
+import io.swagger.annotations.ApiModelProperty;
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private Integer id;
+
+    private String username;
+
+    private Integer age;
+
+    private String password;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", username=").append(username);
+        sb.append(", age=").append(age);
+        sb.append(", password=").append(password);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
